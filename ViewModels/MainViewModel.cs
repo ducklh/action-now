c605755817a2470e84d5bc381432e80f04363823
Vanvanvamaymay <105766971+Vanvanvamaymay@users.noreply.github.com>
2025-03-26 00:23:22 +0700

    init commit

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+using MyWPFApp.Commands;
+
+namespace MyWPFApp.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            // Initialize properties and commands here
+        }
+    }
+} 
